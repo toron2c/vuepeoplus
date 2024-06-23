4b550c87ac733c04f51decd55b6ee0c7a2febc10
toron2c <follik42@gmail.com>
2024-06-23 12:34:43 +0300

    add vuetify, pinia

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,7 @@
 <template>
-  <div>
-    Оставить коментарий
-    <div>
-      name {{ author }}
-      <input
-        name="author"
-        :value="this.author"
-        id="author"
-        @input.prevent="changeNewComment"
-        autocomplete="off"
-      />
-    </div>
-    <div>
-      text {{ message }}
-      <input name="message" type="text" :value="message" id="text" />
-    </div>
-
-    <div>
-      <button>getData</button>
-    </div>
-  </div>
+  <h1>Hello</h1>
 </template>
 
-<script>
-export default {
-  computed: {},
-  data() {
-    return {
-      author: '1',
-      message: '',
-      date: ''
-    }
-  },
-  methods: {
-    changeNewComment(event) {
-      console.log(event)
-    }
-  }
-}
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Vuetify } from './plugins/vuetify'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Vuetify)
 
 app.mount('#app')
